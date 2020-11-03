c4b259abdde56edf49e26486b0f2800f22762954
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-11-03 10:24:16 +0900

    app.js adds

---
+++ app.js
@@ -0,0 +1,16 @@
+// FUNCTIONAL PROGRAMMING 
+
+// function makeAdjective(adjective){
+//     return function (string){
+//         return adjective + " " + string;
+//     }
+// }
+
+// var coolifier = makeAdjective('cool');
+
+
+// console.log(coolifier('conference'));
+
+const year = new Date();
+
+
